a62a664c679fd76ba7dbd7438d4a42cb0cac64b4
Emma Chen <58194727+emmacicichen@users.noreply.github.com>
2022-02-20 23:15:19 -0800

    Update SecurityConfig.java

--- java/com/myproject/FoodNow/SecurityConfig.java
+++ java/com/myproject/FoodNow/SecurityConfig.java
@@ -12,43 +12,36 @@ import javax.sql.DataSource;
 
 @EnableWebSecurity//we got spring security and mvc integration support
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    //WebSecurityConfigurerAdapter是个配置文件的class，定义你去db哪个表查，或者什么url可以访问
 
     @Autowired
-    private DataSource dataSource;//需要访问DB，要注入DataSource
-
+    private DataSource dataSource;
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()//CSRF support，因为我们是单机的，所以把跨域请求的解决方法给disable了
-                .formLogin()//前端会发来表单形式的请求//前端让用户去输入username和pw，相当于给一个/login的API，然后调用查询数据库验证的程序（AuthenticationManagerBuilder那个method）attempAuthentiation
-                .failureForwardUrl("/login?error=true");//当你登陆fail的时候，会redirect到另一个页面//direct到dispatch servlet，然后发给login这个controller
+                .csrf().disable()
+                .formLogin()
+                .failureForwardUrl("/login?error=true");
         http
                 .authorizeRequests()
-                .antMatchers("/order/*", "/cart", "/checkout").hasAuthority("ROLE_USER")//访问/order/*", "/cart", "/checkout"这几个api的时候你必须要有"ROLE_USER"这个权限
-                .anyRequest().permitAll();//剩下其他操作，permit all
+                .antMatchers("/order/*", "/cart", "/checkout").hasAuthority("ROLE_USER")
+                .anyRequest().permitAll();
     }
-    //formLogin()除了login身份验证，失败成功的跳转，他还会产生一个session。 formLogin默认的是session-based -》 见attempAuthentiation方法：从request中读取username和pw，然后调用AuthenticationManager
 
-    @Override//authorization
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {//jdbcAuthentication()//链接数据库的authentication
+    @Override
+    protected void configure(authenticationManagerBuilder auth) throws Exception {
 
         auth
                 .jdbcAuthentication()
-                .dataSource(dataSource)//connect database
-                .usersByUsernameQuery("SELECT email, password, enabled FROM customers WHERE email=?")//在验证用用户名和密码是否正确
-                .authoritiesByUsernameQuery("SELECT email, authorities FROM authorities WHERE email=?");//use '?' to avoid SQL injection
-            //到email password，authorities这些表里找email，返回db里存的密码及一系列信息的 去match，然后保存到session里
-        //去db里找到用户名密码，与浏览器发来的核对 -> if匹配，验证成功，会把这些信息存在session里面
+                .dataSource(dataSource)
+                .usersByUsernameQuery("SELECT email, password, enabled FROM customers WHERE email=?")
+                .authoritiesByUsernameQuery("SELECT email, authorities FROM authorities WHERE email=?");
+            
     }
-    //谁接收login的请求来验证，然后调用数据库数据的呢？AuthenticationManager
-
 
     @SuppressWarnings("deprecation")
     @Bean
-    public static NoOpPasswordEncoder passwordEncoder() {//给密码加密，我们没做，我们密码是明文存在db的，也是明文进行匹配的
+    public static NoOpPasswordEncoder passwordEncoder() {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
-        //也可以在configure中创建一个bean，用一种加密方法加密。框架会把browser发来的明文密码进行匹配，可以自己改这部分
-        //如果想要加密，我们就把NoOpPasswordEncoder变成一种加密方法：比如BCryptPasswordEncoder（见staybooking项目）
     }
 }
